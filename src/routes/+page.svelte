eb648c182c47563297ba0fc77ac078081907d09f
Joshua Gato <joshuagato37@gmail.com>
2022-08-24 10:36:47 +0000

    Replaced fontawesome with google material design icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
-	import Fa from 'svelte-fa';
-	import { faSearch, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 	const data = [
 		{ id: 1, name: 'Irma Flores', date: '16/12/2019', picture: 'profile_1.jpg', description: 'This is the first phrase or clause of my chat.' },
 		{ id: 2, name: 'Irma Flores', date: '16/12/2019', picture: 'profile_2.jpeg', description: 'This is the first phrase or clause of my chat.' },
@@ -27,8 +25,16 @@
 			</span>
 
 			<span class="flex text-gray-500">
-				<span class="mx-3"><Fa size="xl" icon={faSearch} /></span>
-				<span class="mx-3"><Fa icon={faEllipsisVertical} /></span>
+				<span class="mx-3">
+					<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+					</svg>
+				</span>
+				<span class="mx-3">
+					<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+					</svg>
+				</span>
 			</span>
 		</article>
 	</header>
